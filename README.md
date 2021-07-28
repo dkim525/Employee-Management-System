a2a02515416a3305b82f989b50419b1dd687d4a3
Davis Kim <dkim525@wisc.edu>
2021-07-27 21:13:50 -0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Employee-Management-System
+# Portfolio-2
+This is a team profile generator that creates a directory of employees with command line inputs
+
+![Site Screenshot](./website.png)
+
+---
+
+## **Table of Contents**
+
+1. [Links](#Links)
+2. [Installation](#Installation)
+3. [Technologies Used](#Technologies-Used)
+
+---
+
+## **Installation**
+You can start this this application by first:
+
+``npm i``
+
+Then using:
+
+``node index.js``
+
+Run tests:
+
+``npm run test``
+
+## **Links**
+
+[Deployed application](https://dkim525.github.io/Portfolio-2/)\
+[GitHub repository](https://github.com/dkim525/Portfolio-2)
+
+---
+## **Technologies Used**
+* HTML
+* CSS
+* Node.js
+* Jest
---
+++ index.js
@@ -0,0 +1,145 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
+const generateHTML = require('./src/generateHTML');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const members = [];
+
+function managerQuestions() {
+    inquirer
+     .prompt([
+         {
+            type: "input",
+            message: "What is the team manager's name?",
+            name: "name",
+         },
+         {
+            type: "input",
+            message: "What is the team manager's employee ID?",
+            name: "id",
+         },
+         {
+            type: "input",
+            message: "What is the team manager's email address?",
+            name: "email",
+         },
+         {
+            type: "input",
+            message: "What is the team manager's office number?",
+            name: "number",
+         }
+    ])
+     .then((answer) => {
+        const manager = new Manager(answer.name, answer.id, answer.email, answer.number);
+        members.push(manager);
+        optionQuestion();
+    })
+}
+
+function optionQuestion() {
+    inquirer
+     .prompt([
+         {
+           type: "list",
+           message: "Which team member would you like to add?",
+           name: "option",
+           choices: ["Engineer", "Intern", "The team is complete"]
+         }
+     ])
+      .then((choice) => {
+          if (choice.option === "Engineer") {
+              engineerQuestions();
+          } else if (choice.option === "Intern") {
+              internQuestions();
+          } else if (choice.option === "The team is complete") {
+              writeHTML();
+          }
+      })
+}
+
+function engineerQuestions() {
+    inquirer
+     .prompt([
+         {
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "name"
+         },
+         {
+            type: "input",
+            message: "What is the engineer's employee ID?",
+            name: "id"
+         },
+         {
+            type: "input",
+            message: "What is the engineer's email address?",
+            name: "email"
+         },
+         {
+            type: "input",
+            message: "What is the engineer's GitHub username?",
+            name: "github"
+         }
+     ])
+     .then((answer) => {
+        const engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+        members.push(engineer);
+        optionQuestion();
+     })
+}
+
+function internQuestions() {
+    inquirer 
+    .prompt([
+        {
+          type: "input",
+          message: "What is the intern's name?",
+          name: "name"
+        },
+        {
+          type: "input",
+          message: "What is the intern's employee ID?",
+          name: "id"
+        },
+        {
+          type: "input", 
+          message: "What is the intern's email address?",
+          name: "email"
+        },
+        {
+          type: "input", 
+          message: "What is the intern's school?",
+          name: "school"
+        }
+    ])
+    .then((answer) => {
+        const intern = new Intern(answer.name, answer.id, answer.email, answer.school);
+        members.push(intern);
+        optionQuestion();
+    })
+}
+
+
+managerQuestions();
+
+
+function writeToFile(fileName, template) {
+    fs.writeFile(fileName, template, function(err) {
+        console.log(fileName);
+
+        if (err) {
+            return console.log(err);
+        } else {
+            return console.log("Your team is complete!");
+        }
+    })
+}
+
+
+function writeHTML() {
+    let html = generateHTML(members);
+    writeToFile('./dist/team.html', html);
+}
